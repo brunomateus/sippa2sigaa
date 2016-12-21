61d1158838953684207af134696cbf59c1c342f8
Bruno Mateus <brunomateus@gmail.com>
2016-12-21 00:06:28 -0300

    Armazenando informacoes de frequencia

--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,34 @@
 var step = -1;
 
-function carregar_frequencia(e){
+
+function carregar_frequencia_from_sippa(e){
 
     chrome.tabs.executeScript({file: "js/jquery-3.1.1.min.js"}, function(){
         chrome.tabs.executeScript({file:"js/extract_freq.js"}, function(resultado){
-            var exportar = $("#exportar");
-            exportar.empty();
-            exportar.append("Disciplina " +  resultado[0].disciplina);
-            var table = $("<table></table>").attr("id", "dados_alunos");
-            var dados = resultado[0].dados;
-            for(var i = 0; i < dados.length; i++){
-                var row = $("<tr></tr>").attr("id", dados[i]["matricula"]);
-                for(var label in dados[i]){
-                    var cell = $("<td></td>").text(dados[i][label]);
-                    row.prepend(cell);
-                }
-                table.append(row);
-            }
-            exportar.append(table);
-            step = 2;
+            carregar_frequencia_from_json(resultado[0]);
+            chrome.storage.local.set({"dados": resultado[0]}, function(){
+                console.log("Informações salvas");
+            });
         });
     });
+}
+
+function carregar_frequencia_from_json(resultado){
+    var exportar = $("#exportar");
+    exportar.empty();
+    exportar.append("Disciplina " +  resultado.disciplina);
+    var table = $("<table></table>").attr("id", "dados_alunos");
+    var dados = resultado.dados;
+    for(var i = 0; i < dados.length; i++){
+        var row = $("<tr></tr>").attr("id", dados[i]["matricula"]);
+        for(var label in dados[i]){
+            var cell = $("<td></td>").text(dados[i][label]);
+            row.prepend(cell);
+        }
+        table.append(row);
+    }
+    exportar.append(table);
+    step = 2;
 }
 
 function carregar_nota(e){
@@ -36,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
     chrome.tabs.getSelected(null, function(tab){
         console.log("Aba selecionada " +  tab.url);
         if(tab.url == "https://sistemas.quixada.ufc.br/sippa/professor_visualizar_turma.jsp"){
-            loadFreq.on('click', carregar_frequencia);
+            loadFreq.on('click', carregar_frequencia_from_sippa);
             loadFreq.prop('disabled', false);
         } else {
             loadFreq.prop('disabled', true);
@@ -49,6 +57,12 @@ document.addEventListener('DOMContentLoaded', function () {
             loadNota.prop('click', true);
         }
 
+    });
+
+    var dados_salvos = chrome.storage.local.get("dados", function(resultado){
+        if(resultado.dados){
+            carregar_frequencia_from_json(resultado.dados);
+        }
     });
 
 });
